da4201292c006a1bed9b22e5dcb40aa9b2916879
Shaun Hurley <shaunhurley@hotmail.com>
2020-07-21 10:10:32 -0400

    Initial Commit

---
+++ TestPermissionsRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPermissionsRequest
+//
+//  Created by Shaun on 7/21/20.
+//  Copyright © 2020 Shaun Hurley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
